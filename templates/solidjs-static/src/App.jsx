90821b4999e71a445fa55e64f689f8b2e2104073
zhanghan <Lancelot_Lewis@163.com>
2022-04-28 23:29:39 +0800

    feat: add solidjs-static template

---
+++ templates/solidjs-static/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://docs.arcblock.io/abtnode/en/blocklet"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn Blocklet
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
