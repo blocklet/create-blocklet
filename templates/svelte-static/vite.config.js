460c3b543f64530d8611c8f89a98cb14ae3530ba
zhanghan <Lancelot_Lewis@163.com>
2022-04-29 16:18:42 +0800

    feat: add svelte-static template

---
+++ templates/svelte-static/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { createBlockletPlugin } from 'vite-plugin-blocklet';
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  const envMap = loadEnv(mode, process.cwd(), '');
+  const port = process.env.BLOCKLET_PORT || 3000;
+
+  return defineConfig({
+    plugins: [
+      svelte(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            base: process.env.BASE_URL || '/',
+            title: envMap.APP_TITLE,
+          },
+        },
+      }),
+      createBlockletPlugin(),
+    ],
+    server: {
+      port,
+    },
+  });
+};
