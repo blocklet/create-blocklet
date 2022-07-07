16003cb48b5470b4dc6ec8e423e48e6431deaf68
LiangYongzhuo <727471643@qq.com>
2022-07-07 15:43:35 +0800

    feat: support the templates of vue-cli dev in the component mode (#135)

--- packages/create-app/templates/vue2-dapp/vue.config.js
+++ packages/create-app/templates/vue2-dapp/vue.config.js
@@ -3,14 +3,18 @@ require('dotenv-flow').config();
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3000;
 const apiPort = process.env.API_PORT || 3030;
 
+const whenDev = process.env.NODE_ENV === 'development';
+
+const mountPoint = process.env.BLOCKLET_DEV_MOUNT_POINT || '';
+
 module.exports = {
-  publicPath: process.env.PUBLIC_PATH || '/',
+  publicPath: whenDev ? '' : process.env.PUBLIC_PATH || '/',
   devServer: {
     port,
     allowedHosts: 'all',
     client: {
       // If you want to development this blocklet without blocklet-server, you can delete next line, otherwise the hot reload will be failed.
-      webSocketURL: 'wss://0.0.0.0/ws',
+      webSocketURL: `wss://0.0.0.0${mountPoint}/ws`,
     },
     proxy: {
       '/api': {
--- packages/create-app/templates/vue2-static/vue.config.js
+++ packages/create-app/templates/vue2-static/vue.config.js
@@ -1,13 +1,17 @@
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3000;
 
+const whenDev = process.env.NODE_ENV === 'development';
+
+const mountPoint = process.env.BLOCKLET_DEV_MOUNT_POINT || '';
+
 module.exports = {
-  publicPath: process.env.PUBLIC_PATH || '/',
+  publicPath: whenDev ? '' : process.env.PUBLIC_PATH || '/',
   devServer: {
     port,
     allowedHosts: 'all',
     client: {
       // If you want to development this blocklet without blocklet-server, you can delete next line, otherwise the hot reload will be failed.
-      webSocketURL: 'wss://0.0.0.0/ws',
+      webSocketURL: `wss://0.0.0.0${mountPoint}/ws`,
     },
   },
 };
