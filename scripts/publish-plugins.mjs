05e91a4e0e36df3ccc7edcb985bd73e69fbc30ba
Yongzhuo Liang <firetable@foxmail.com>
2022-07-09 01:45:07 +0800

    chore: update publish-plugins.mjs details

--- scripts/publish-plugins.mjs
+++ scripts/publish-plugins.mjs
@@ -4,8 +4,9 @@ import { fs } from 'zx';
 
 const pluginList = await fs.readdirSync(`./plugins`); // scan the plugins directory
 
-const pathList = pluginList.map((plugin) => `./plugins/${plugin}`);
+const pathList = pluginList.filter((name) => !name.startsWith('.')).map((pluginName) => `./plugins/${pluginName}`);
 
 pathList.map((path) => {
-  execSync(`npm publish ${path}  --access public --no-git-checks`, { stdio: 'inherit' });
+  execSync(`cd ${path} && npm run build`, { stdio: 'inherit' });
+  execSync(`npm publish ${path} --access public --no-git-checks`, { stdio: 'inherit' });
 });
