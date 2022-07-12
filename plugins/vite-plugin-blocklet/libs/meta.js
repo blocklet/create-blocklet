a5157605f98b51cdd7dca34f394e87d610b49adc
zhanghan <Lancelot_Lewis@163.com>
2022-07-12 22:33:48 +0800

    feat: add vite-plugin-blocklet

---
+++ plugins/vite-plugin-blocklet/libs/meta.js
@@ -0,0 +1,19 @@
+export default function createMetaPlugin() {
+  return {
+    name: 'blocklet:meta',
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            // injectTo: 'head',
+            tag: 'script',
+            attrs: {
+              src: '__meta__.js',
+            },
+          },
+        ],
+      };
+    },
+  };
+}
