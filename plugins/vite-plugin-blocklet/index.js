8b47c3cf1803de536e6ea5517fd2717483284581
zhanghan <Lancelot_Lewis@163.com>
2022-07-19 10:27:15 +0800

    fix: add "dist" in vite-plugin-blocklet

--- plugins/vite-plugin-blocklet/index.js
+++ plugins/vite-plugin-blocklet/index.js
@@ -1,7 +1,6 @@
 import createHmrPlugin from './libs/hmr.js';
 import createConfigPlugin from './libs/config.js';
 import createMetaPlugin from './libs/meta.js';
-import { isInBlocklet } from './libs/utils.js';
 
 export function createBlockletPlugin(options = {}) {
   const { disableConfig = false, disableMeta = false, disableHmr = false } = options;
@@ -12,11 +11,13 @@ export function createBlockletPlugin(options = {}) {
   if (!disableConfig) {
     plugins.push(createConfigPlugin(options));
   }
-  if (isInBlocklet) {
-    if (!disableHmr) {
-      plugins.push(createHmrPlugin(options));
-    }
+  if (!disableHmr) {
+    plugins.push(createHmrPlugin(options));
   }
 
   return plugins;
 }
+
+export const createBlockletHmr = createHmrPlugin;
+export const createBlockletConfig = createConfigPlugin;
+export const createBlockletMeta = createMetaPlugin;
