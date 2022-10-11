1bc209cb7ca81a54b3877a4b8a228e7178b3262f
Yongzhuo Liang <firetable@foxmail.com>
2022-10-11 13:44:23 +0800

    chore: react-static

---
+++ packages/create-app/templates/react-static/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { createBlockletPlugin } from 'vite-plugin-blocklet';
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ mode }) => {
+  const envMap = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: envMap.APP_TITLE,
+          },
+        },
+      }),
+      createBlockletPlugin(),
+    ],
+  };
+});
