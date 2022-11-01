a59529e598d156a341b32acc2ea3d2313f8a9a51
zhanghan <Lancelot_Lewis@163.com>
2022-11-01 17:14:47 +0800

    feat: vite-plugin-blocklet can inject title automatic

--- plugins/vite-plugin-blocklet/libs/meta.js
+++ plugins/vite-plugin-blocklet/libs/meta.js
@@ -1,18 +1,34 @@
+import fs from 'fs';
+import YAML from 'yaml';
+
 export default function createMetaPlugin() {
   return {
     name: 'blocklet:meta',
     transformIndexHtml(html) {
+      const tags = [];
+      // 如果 index.html 中没有设置 title，则自动注入 blocklet.yml 中的 title
+      if (!/<title>(.*?)<\/title>/.test(html)) {
+        const blockletYamlPath = './blocklet.yml';
+        const blockletYaml = YAML.parse(fs.readFileSync(blockletYamlPath, 'utf8'));
+        const { title } = blockletYaml;
+
+        tags.push({
+          tag: 'title',
+          children: title,
+        });
+      }
+      // 默认注入 __blocklet__.js 文件
+      tags.push({
+        // injectTo: 'head',
+        tag: 'script',
+        attrs: {
+          src: '__blocklet__.js',
+        },
+      });
+
       return {
         html,
-        tags: [
-          {
-            // injectTo: 'head',
-            tag: 'script',
-            attrs: {
-              src: '__blocklet__.js',
-            },
-          },
-        ],
+        tags,
       };
     },
   };
