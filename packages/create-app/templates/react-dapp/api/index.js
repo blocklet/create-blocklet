2b2709ffce72c513d9eb065604f8a75fb2dd8bce
zhanghan <Lancelot_Lewis@163.com>
2022-11-01 17:40:48 +0800

    chore: update react-dapp template

--- packages/create-app/templates/react-dapp/api/index.js
+++ packages/create-app/templates/react-dapp/api/index.js
@@ -16,36 +16,34 @@ app.set('trust proxy', true);
 app.use(cookieParser());
 app.use(express.json({ limit: '1 mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1 mb' }));
+app.use(cors());
 
 const router = express.Router();
 router.use('/api', require('./routes'));
+app.use(router);
 
-const isDevelopment = process.env.NODE_ENV === 'development';
 const isProduction = process.env.NODE_ENV === 'production' || process.env.ABT_NODE_SERVICE_ENV === 'production';
 
 if (isProduction) {
-  app.use(cors());
+  const staticDir = path.resolve(__dirname, '../dist');
   app.use(compression());
-
-  const staticDir = path.resolve(process.env.BLOCKLET_APP_DIR, 'dist');
   app.use(express.static(staticDir, { maxAge: '30d', index: false }));
-  app.use(router);
   app.use(fallback('index.html', { root: staticDir }));
 
-  app.use((req, res) => {
-    res.status(404).send('404 NOT FOUND');
-  });
   app.use((err, req, res) => {
     logger.error(err.stack);
     res.status(500).send('Something broke!');
   });
-} else {
-  app.use(router);
 }
 
-const port = (isDevelopment ? parseInt(process.env.API_PORT, 10) : parseInt(process.env.BLOCKLET_PORT, 10)) || 3030;
+const port = parseInt(process.env.BLOCKLET_PORT, 10);
 
-app.listen(port, (err) => {
+const server = app.listen(port, (err) => {
   if (err) throw err;
   logger.info(`> ${name} v${version} ready on ${port}`);
 });
+
+module.exports = {
+  app,
+  server,
+};
--- plugins/vite-plugin-blocklet/libs/hmr.js
+++ plugins/vite-plugin-blocklet/libs/hmr.js
@@ -1,4 +1,5 @@
 import { version as viteVersion } from 'vite';
+import semver from 'semver';
 import { isInBlocklet } from './utils.js';
 
 export default function createHmrPlugin({ version = viteVersion } = {}) {
@@ -7,15 +8,16 @@ export default function createHmrPlugin({ version = viteVersion } = {}) {
     apply: 'serve',
     async transform(code, id) {
       if (isInBlocklet && id.endsWith('/vite/dist/client/client.mjs')) {
+        const pureVersion = semver.major(version);
         let replacedCode = code;
-        if (version === 2) {
+        if (pureVersion === 2) {
           replacedCode = replacedCode.replace("const base = __BASE__ || '/';\n", '');
           replacedCode = replacedCode.replace(
             'const socketHost = `${__HMR_HOSTNAME__ || location.hostname}:${__HMR_PORT__}`;',
             "const base = __BASE__ || '/';\nlet tmpPort = __HMR_PORT__;\nif (window.blocklet) {\ntmpPort = new URL(window.location.href).port + base;\n}\nconst socketHost = `${__HMR_HOSTNAME__ || location.hostname}${tmpPort ? `:${tmpPort}` : ''}`;"
           );
           return replacedCode;
-        } else if (version === 3) {
+        } else if (pureVersion === 3) {
           let replacedCode = code;
           replacedCode = replacedCode.replace('__HMR_PORT__', 'undefined');
           return replacedCode;
