46fc587db1b2b4bf480aa875a6265f9ceeb23914
zhanghan <Lancelot_Lewis@163.com>
2022-11-02 11:54:29 +0800

    chore: fix typo

--- packages/create-app/templates/react-static/vite.config.js
+++ packages/create-app/templates/react-static/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 import svgr from 'vite-plugin-svgr';
