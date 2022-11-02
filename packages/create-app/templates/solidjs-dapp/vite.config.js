aaeaa1ec1b1f776b6d96f3920fc745e7c1e50289
zhanghan <Lancelot_Lewis@163.com>
2022-11-02 10:22:42 +0800

    chore: update solidjs-dapp template

--- packages/create-app/templates/solidjs-dapp/vite.config.js
+++ packages/create-app/templates/solidjs-dapp/vite.config.js
@@ -1,33 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 
-export default defineConfig(async ({ mode }) => {
-  const envMap = loadEnv(mode, process.cwd(), '');
-  const apiPort = envMap.API_PORT || 3030;
-
+export default defineConfig(() => {
   return {
-    plugins: [
-      solidPlugin(),
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            title: envMap.APP_TITLE,
-          },
-        },
-      }),
-      createBlockletPlugin(),
-    ],
+    plugins: [solidPlugin(), createBlockletPlugin()],
     build: {
       target: 'esnext',
       polyfillDynamicImport: false,
-    },
-    server: {
-      proxy: {
-        '/api': `http://127.0.0.1:${apiPort}`,
-      },
     },
   };
 });
