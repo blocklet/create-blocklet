14e5b9f21127e53ee01097252448c8a60af243af
zhanghan <Lancelot_Lewis@163.com>
2022-11-02 11:13:37 +0800

    chore: update xxx-static template, remove vite-plugin-html

--- packages/create-app/templates/solidjs-static/vite.config.js
+++ packages/create-app/templates/solidjs-static/vite.config.js
@@ -1,24 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 
-export default defineConfig(async ({ mode }) => {
-  const envMap = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return {
-    plugins: [
-      solidPlugin(),
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            title: envMap.APP_TITLE,
-          },
-        },
-      }),
-      createBlockletPlugin(),
-    ],
+    plugins: [solidPlugin(), createBlockletPlugin()],
     build: {
       target: 'esnext',
       polyfillDynamicImport: false,
--- packages/create-app/templates/vue-static/vite.config.js
+++ packages/create-app/templates/vue-static/vite.config.js
@@ -1,24 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 
 // https://vitejs.dev/config/
-export default defineConfig(async ({ mode }) => {
-  const envMap = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return {
-    plugins: [
-      vue(),
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            title: envMap.APP_TITLE,
-          },
-        },
-      }),
-      createBlockletPlugin(),
-    ],
+    plugins: [vue(), createBlockletPlugin()],
   };
 });
