e9fe2fea3e06949a35f0fa16e24d10384746c42b
zhanghan <Lancelot_Lewis@163.com>
2022-11-04 21:58:16 +0800

    fix: update libs/client import

--- plugins/vite-plugin-blocklet/index.js
+++ plugins/vite-plugin-blocklet/index.js
@@ -1,7 +1,7 @@
 import createHmrPlugin from './libs/hmr.js';
 import createConfigPlugin from './libs/config.js';
 import createMetaPlugin from './libs/meta.js';
-import setupClient from './libs/client';
+import setupClient from './libs/client.js';
 
 export function createBlockletPlugin(options = {}) {
   const { disableConfig = false, disableMeta = false, disableHmr = false } = options;
