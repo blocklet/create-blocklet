14417611e4302cfab4b8af0147dade4477d60eff
zhanghan <lancelot_lewis@163.com>
2023-01-29 19:59:24 +0800

    feat: setupClient support pass confiFile params

--- plugins/vite-plugin-blocklet/libs/client.js
+++ plugins/vite-plugin-blocklet/libs/client.js
@@ -1,14 +1,22 @@
 import getPort from 'get-port';
 import { createServer } from 'vite';
+import mri from 'mri';
 
+const argv = process.argv.slice(2);
 const isProduction = process.env.NODE_ENV === 'production' || process.env.ABT_NODE_SERVICE_ENV === 'production';
 
 export default async function setupClient(app, options = {}) {
   if (!isProduction) {
-    const { host = '127.0.0.1', protocol = 'ws', port: inputPort } = options;
+    const params = mri(argv, {
+      alias: {
+        config: 'c',
+      },
+    });
+    const { host = '127.0.0.1', protocol = 'ws', port: inputPort, configFile = '' } = options;
     const port = await getPort({ port: inputPort });
     // 以中间件模式创建 Vite 服务器
     const vite = await createServer({
+      configFile: params.config || configFile || undefined,
       server: {
         middlewareMode: true,
         hmr: {
