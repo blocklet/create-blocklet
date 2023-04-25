8ba4c4feb84c86af110a0770566cecfa1a06fa4f
zhanghan <lancelot_lewis@163.com>
2023-04-25 10:53:50 +0800

    chore: disableNodePolyfills default -> false

--- plugins/vite-plugin-blocklet/index.js
+++ plugins/vite-plugin-blocklet/index.js
@@ -7,7 +7,8 @@ import setupClient from './libs/client.js';
 export function createBlockletPlugin(options = {}) {
   const {
     // NOTICE: 由于 polyfill 不是每个项目都必须的，并且有些现有的项目已经配置了 polyfill，所以这个配置默认 disable 会比较好
-    disableNodePolyfills = true,
+    // UPDATE: 经过实践，大部分项目都需要此配置，现变更为默认开启 @2023-04-25
+    disableNodePolyfills = false,
     disableConfig = false,
     disableMeta = false,
     disableHmr = false,
