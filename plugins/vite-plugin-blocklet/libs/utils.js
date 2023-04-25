6cdcad3ae199aa7b1950725c8ca81697833dc73b
zhang han <LancelotLewis@users.noreply.github.com>
2023-04-25 14:38:59 +0800

    chore: update deps (#281)
    Co-authored-by: linchen1987 <linchen.1987@foxmail.com>

--- plugins/vite-plugin-blocklet/libs/utils.js
+++ plugins/vite-plugin-blocklet/libs/utils.js
@@ -1,10 +1,14 @@
 import Mcrypto from '@ocap/mcrypto';
 import { toHex } from '@ocap/util';
-import { fromPublicKey } from '@arcblock/did';
+import { fromPublicKey, isValid } from '@arcblock/did';
 
 const { types } = Mcrypto;
 
 export function toBlockletDid(name) {
+  if (isValid(name)) {
+    return name;
+  }
+
   const pk = toHex(Buffer.from(typeof name === 'string' ? name.trim() : name));
   return fromPublicKey(pk, { role: types.RoleType.ROLE_ANY });
 }
