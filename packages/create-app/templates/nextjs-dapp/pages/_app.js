a7decb49f2b5f61c51d50d59ed7d1c9fc0967e4d
wangshijun <wangshijun2010@gmail.com>
2023-08-24 07:33:02 +0800

    feat: remove meta description in favor of server injected

--- packages/create-app/templates/nextjs-dapp/pages/_app.js
+++ packages/create-app/templates/nextjs-dapp/pages/_app.js
@@ -10,7 +10,6 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico?imageFilter=resize&w=32" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <meta name="theme-color" content="#4F6AF5" />
-        <meta name="description" content="Web site created using create-blocklet" />
       </Head>
       <Script src="__blocklet__.js" />
       <Component {...pageProps} />
