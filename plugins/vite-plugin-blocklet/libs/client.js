cb4aa880125a9dd53fdb47ef003077921cc16b00
zhanghan <lancelot_lewis@163.com>
2024-03-25 14:59:13 +0800

    feat: support cache vite port in .env.development.local file

--- plugins/vite-plugin-blocklet/libs/client.js
+++ plugins/vite-plugin-blocklet/libs/client.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import getPort from 'get-port';
 import { createServer } from 'vite';
 import mri from 'mri';
@@ -25,7 +27,28 @@ export default async function setupClient(app, options = {}) {
       },
     });
     const { host = '127.0.0.1', protocol = 'ws', port: inputPort, configFile = '', appType = 'spa' } = options;
-    const port = await getPort({ port: inputPort });
+    let skipWritePort = true;
+    let envAppendContent = '';
+    const envFile = path.join(process.cwd(), '.env.development.local');
+    let port;
+
+    if (!fs.existsSync(envFile)) {
+      skipWritePort = false;
+      port = await getPort({ port: inputPort });
+      envAppendContent = `VITE_BLOCKLET_PORT=${port}`;
+    } else {
+      port = await getPort({ port: inputPort });
+      const content = await fs.promises.readFile(envFile, 'utf-8');
+      if (!content.includes('VITE_BLOCKLET_PORT')) {
+        skipWritePort = false;
+        envAppendContent = `${content}\nVITE_BLOCKLET_PORT=${port}`;
+      } else {
+        port = process.env.VITE_BLOCKLET_PORT;
+      }
+    }
+    if (!skipWritePort && envAppendContent) {
+      await fs.promises.writeFile(envFile, envAppendContent);
+    }
     // 以中间件模式创建 Vite 服务器
     const vite = await createServer({
       configFile: params.config || configFile || undefined,
