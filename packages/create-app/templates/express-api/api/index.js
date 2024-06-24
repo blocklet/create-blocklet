0e9ddf4dafa848ed21a195914093220e2f45cfcd
zhanghan <lancelot_lewis@163.com>
2024-06-25 01:55:02 +0800

    chore(template): update express-api template

--- packages/create-app/templates/express-api/api/index.js
+++ packages/create-app/templates/express-api/api/index.js
@@ -5,18 +5,8 @@ const app = express();
 
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3030;
 
-app.get('/', (req, res) => {
-  res.send(`
-<div style="display:flex;flex-direction:column;align-items:center;padding:64px 0;">
-<h1 style="margin:64px 0;">
-  <span style="display:inline-block;padding:8px 24px;background:#1dc1c7;color:#fff;">Blocklet</span>
-  <span style="color:#777;">+ Express</span>
-</h1>
-<pre>
-${JSON.stringify(env, null, 2)}
-</pre>
-</div>
-  `);
+app.get('/', (_, res) => {
+  res.send('Hello from Blocklet app!');
 });
 
 app.listen(port, () => {
