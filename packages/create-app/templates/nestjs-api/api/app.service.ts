cf864428cc2903df376ccdce1286b730a3404816
zhanghan <lancelot_lewis@163.com>
2024-06-25 01:57:58 +0800

    chore(template): update nestjs-api template

--- packages/create-app/templates/nestjs-api/api/app.service.ts
+++ packages/create-app/templates/nestjs-api/api/app.service.ts
@@ -8,16 +8,6 @@ const environment = {
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `
-    <div style="display:flex;flex-direction:column;align-items:center;padding:64px 0;">
-    <h1 style="margin:64px 0;">
-      <span style="display:inline-block;padding:8px 24px;background:#1dc1c7;color:#fff;">Blocklet</span>
-      <span style="color:#777;">+ NestJS</span>
-    </h1>
-    <pre>
-    ${JSON.stringify(environment, null, 2)}
-    </pre>
-    </div>
-      `;
+    return 'Hello from Blocklet app!';
   }
 }
