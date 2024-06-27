119f7adf8af354c00f3c4a057d8ef68b04ec8402
zhanghan <lancelot_lewis@163.com>
2024-06-27 11:44:30 +0800

    chore(template): update solidjs-static template

--- packages/create-app/templates/solidjs-static/.eslintrc.js
+++ packages/create-app/templates/solidjs-static/.eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 module.exports = {
   root: true,
   plugins: ['solid'],
@@ -5,5 +6,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  parserOptions: {
+    sourceType: 'module',
   },
 };
