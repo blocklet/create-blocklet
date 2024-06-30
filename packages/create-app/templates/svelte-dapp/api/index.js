51bd185b471aa609edeb6b003f06e4a99898d548
zhanghan <lancelot_lewis@163.com>
2024-06-30 18:40:23 +0800

    fix(template): fix lint error

--- packages/create-app/templates/svelte-dapp/api/index.js
+++ packages/create-app/templates/svelte-dapp/api/index.js
@@ -19,6 +19,7 @@ app.use(cors());
 
 const router = express.Router();
 router.use('/api', require('./routes'));
+
 app.use(router);
 
 const isProduction = process.env.NODE_ENV === 'production' || process.env.ABT_NODE_SERVICE_ENV === 'production';
