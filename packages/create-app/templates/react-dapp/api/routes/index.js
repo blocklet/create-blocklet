67798023956741eef239f2c7ee2164cdcea18df8
zhanghan <lancelot_lewis@163.com>
2024-11-13 01:02:05 +0800

    fix(create-app): fix typo

--- packages/create-app/templates/react-dapp/api/routes/index.js
+++ packages/create-app/templates/react-dapp/api/routes/index.js
@@ -1,4 +1,4 @@
-const middlewares = require('@blocklet/sdk/lib/middlewaress');
+const middlewares = require('@blocklet/sdk/lib/middlewares');
 const router = require('express').Router();
 
 router.use('/user', middlewares.session(), (req, res) => res.json(req.user || {}));
