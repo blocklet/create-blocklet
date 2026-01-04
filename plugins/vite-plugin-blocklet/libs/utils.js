19314f66630fd2f7ee002edec2314a8be344ff61
Lancelot Lewis <lancelot_lewis@163.com>
2026-01-04 21:04:26 +0800

    fix(plugin-blocklet): fix import @ocap/mcrypto

--- plugins/vite-plugin-blocklet/libs/utils.js
+++ plugins/vite-plugin-blocklet/libs/utils.js
@@ -1,10 +1,8 @@
 import { promises as fs } from 'node:fs';
 import YAML from 'yaml';
-import Mcrypto from '@ocap/mcrypto';
+import { types } from '@ocap/mcrypto';
 import { toHex } from '@ocap/util';
 import { fromPublicKey, isValid } from '@arcblock/did';
-
-const { types } = Mcrypto;
 
 export function toBlockletDid(name) {
   if (isValid(name)) {
